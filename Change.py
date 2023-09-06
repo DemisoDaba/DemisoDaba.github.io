f889504ba9d475139b66680542a1bed3988c4186
Demiso Daba <125874545+DemisoDaba@users.noreply.github.com>
2023-09-06 21:25:35 +0300

    Create Change.py

---
+++ Change.py
@@ -0,0 +1,16 @@
+# Define program details
+program_name = "#ALX_SE"
+sprint_number = 2
+months_completed = 6
+organization = "@alx_africa"
+
+# Create a message
+message = (
+    f"I've completed half of the {program_name} program,"
+    f"\nconcluding Sprint {sprint_number},"
+    f"\nand I can confidently say that I'm a completely different person than when I began."
+    f"\nI want to express my gratitude to {organization} for this transformative journey."
+)
+
+# Print the message
+print(message)
